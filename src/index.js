a5fdae404f081899fe35b5ea2fb5dd0f29988002
Ian Cossentino <icossentino@gmail.com>
2021-04-07 15:45:20 -0400

    edited index.js to use thunk, redux store, provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
