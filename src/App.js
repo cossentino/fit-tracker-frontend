e8349478b9de60f321df634dcd43bfc5fdb7ad64
Ian Cossentino <icossentino@gmail.com>
2021-04-07 17:17:43 -0400

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Home from './components/Home'
 import HomeContainer from './containers/homeContainer'
+import NavBar from './components/NavBar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar />
       <HomeContainer />
     </div>
   );
