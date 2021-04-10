763009ba457bc147789aae7c76527650baa2f74d
Ian Cossentino <icossentino@gmail.com>
2021-04-10 15:28:51 -0400

    deleted commented out text

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -32,40 +32,4 @@ const Workout = props => {
   )
 }
 
-
-
 export default Workout
-
-
-
-
-
-// const test = () => {
-//   return (
-//     <div class="col s12 m7">
-//       <div class="card horizontal">
-//        <div class="card-image">
-//          <img src="https://lorempixel.com/100/190/nature/6" alt="test"/>
-//       </div>
-//       <div class="card-stacked">
-//         <div class="card-content">
-//           <span className="card-title">{`${capitalizeWord(myWorkout.workout_type)} - ${formatDate(myWorkout.date)}`}</span>
-//             <p>Miles: {myWorkout.miles}</p>
-//             <p>Perceived Exertion: {myWorkout.exert}</p>
-//             <p>Pace: {myWorkout.pace}</p>
-//             <p>Notes: {myWorkout.notes}</p>
-//         </div>
-//         <div class="card-action">
-//           <button 
-//             className="btn" 
-//             onClick={() => {
-//               props.delete(props.workout.id)
-//             }}
-//           >Delete Workout</button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-
-//   )
-// }
