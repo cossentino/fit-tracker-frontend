c5bcb3292a77ae7be5018127b7110c3b5e7325ba
Ian Cossentino <icossentino@gmail.com>
2021-04-10 12:37:43 -0400

    added homepage stub

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LoginForm from './components/Sessions/LoginForm'
 import CreateUserForm from './components/Sessions/CreateUserForm'
 import React from 'react'
 import { connect } from 'react-redux'
+import Dashboard from './components/dashboard/Dashboard';
 
 const App = (props) => {
 
@@ -15,6 +16,7 @@ const App = (props) => {
       <div className="App">
         <NavBar loggedIn={props.loggedIn} />
         <Switch>
+          <Route path="/" exact component={Dashboard} />
           <Route path="/workouts" exact component={WorkoutsContainer} />
           <Route path="/workouts/new" exact component={WorkoutChoice} />
           <Route path="/workouts/create/:type" exact render={() => <CreateWorkoutForm />} />
---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+class Dashboard extends Component {
+
+  render() {
+    return(<div className="container" id="dashboard">
+      <h3>My Dashboard</h3>
+    </div>)
+  }
+}
+
+
+export default Dashboard
