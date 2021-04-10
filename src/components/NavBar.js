908e98d979c9e1111902fa22b2db4a4b34ff79e8
Ian Cossentino <icossentino@gmail.com>
2021-04-10 12:34:38 -0400

    arb commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,15 +10,16 @@ const NavBar = props => {
       onClick={() => {
       localStorage.clear()
       store.dispatch({type: 'LOG_OUT'})
-      }}
-      ><Link to="/login">Log Out</Link>
-    </button>
+      }}>
+        <Link to="/login">Log Out</Link>
+      </button>
   )}
 
   return (
     <nav style={{backgroundColor: '#0647C8'}}>
       <div className="nav-wrapper container">
         <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to="/">Home</Link></li>
           <li><Link to="/workouts">Workouts</Link></li>
           <li><Link to="/workouts/new">Log Workout</Link></li>
           {props.loggedIn ? logOutButton() : <li><Link to="/login">Log In</Link></li>}
