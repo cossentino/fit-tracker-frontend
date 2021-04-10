7e07617d4c0f9ee620b1b8eae00b99b852396125
Ian Cossentino <icossentino@gmail.com>
2021-04-10 12:08:13 -0400

    refactored workout choice to pure component

--- src/components/WorkoutChoice.js
+++ src/components/WorkoutChoice.js
@@ -1,50 +1,30 @@
 // import M from "materialize-css"
-import React, { Component } from 'react'
-// import { postConfObj, currentUser } from '../library'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { currentUser } from '../library'
+import { Link, Redirect } from 'react-router-dom'
 import bike from '../images/bike.png'
 import run from '../images/run.png'
 import swim from '../images/swim.png'
 
 
+const WorkoutChoice = () => {
 
+  const myUser = currentUser()
 
-
-
-
-class WorkoutChoice extends Component {
-
-  constructor() {
-    super()
-    this.state = {
-    }
-  }
-
-  // handleClick = e => {
-
-
-
-  render() {
     return (
       <div className="row container workout-choice">
-        <h3 style={{paddingBottom: '30px'}}>Choose a Workout Type: </h3>
-        <div className="row">
-          <Link to="/workouts/create/run"><img style={{height: "250px"}} src={run} alt="runner" /></Link>
-          <Link to="/workouts/create/swim"><img style={{height: "250px", float: 'right'}} src={swim} alt="swimmer" /></Link>
-          <Link to="/workouts/create/bike"><img style={{height: "250px", float: 'right'}} src={bike} alt="cyclist" /></Link>
-        </div>
+        {myUser ?
+          <div>
+            <h3 style={{paddingBottom: '30px'}}>Choose a Workout Type: </h3>
+            <div className="row">
+              <Link to="/workouts/create/run"><img style={{height: "250px"}} src={run} alt="runner" /></Link>
+              <Link to="/workouts/create/swim"><img style={{height: "250px", float: 'right'}} src={swim} alt="swimmer" /></Link>
+              <Link to="/workouts/create/bike"><img style={{height: "250px", float: 'right'}} src={bike} alt="cyclist" /></Link>
+            </div>
+          </div> 
+          : <Redirect to="/login" />}
       </div>
       )
-  }
-
-
-    
-
-
 }
-
-
-
-
 
 export default WorkoutChoice
