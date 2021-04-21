f0b70a74578792d5eed915e183713899c7f71162
Ian Cossentino <icossentino@gmail.com>
2021-04-21 15:55:13 -0400

    stylistic progress bar changes

--- src/components/dashboard/ProgressBar.js
+++ src/components/dashboard/ProgressBar.js
@@ -8,17 +8,19 @@ const ProgressBar = props => {
     <div>
       { props.goal ?
       <div>
-        <h5>This month's goal: </h5>
-        <div class="container">
-          <p>{props.goal.attributes.description}</p>
-          <p>Goal mileage: {props.goal.attributes.miles}</p>
-          <p>Mileage This Month: {currentMonthMilesBySport(props.goal)}</p>
+        <div id="goal_information" >
+          <h5 style={{marginTop: '50px', display: 'inline-block' }}>This month's goal: {props.goal.attributes.description} </h5>
+          <div class="container">
+            <p>Goal mileage: {props.goal.attributes.miles}</p>
+            <p>Mileage This Month: {currentMonthMilesBySport(props.goal)}</p>
+          </div>
         </div>
-        <h6>You are {progressPercentage(props.goal)} of the way toward this month's goal!</h6>
-        <div className="progress">
+        <h6>You are {progressPercentage(props.goal)} of the way toward your goal!</h6>
+        <div className="progress" style={{marginBottom: '50px', display: 'inline-block' }}>
           <div className="determinate" style={{width: progressPercentage(props.goal)}}></div>
         </div>
-      </div> : null }
+      </div>
+       : null }
     </div>
   )
 }
